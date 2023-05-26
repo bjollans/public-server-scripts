5c35bca3477b0fa4bbd1e807b3cabc8206a37b8e
Bernard Jollans <jollans94@googlemail.com>
2023-05-26 10:53:45 +0200

    Made checking out the correct branch one click
    Did not test this so might need another update

--- clone_repo.sh
+++ clone_repo.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
 URL=$1
+shift
+BRANCH=$1
+shift
 
-if [[ -z $URL ]]; then
-    echo "This script takes a single argument, namely the github repo you want to clone. No URL provided. Exiting"
-    exit 1
+check_argument() {
+    if [[ -z "$1" ]]; then
+        help_and_exit
+    fi
+}
+
+help_and_exit() {
+    echo "Usage: $0 <url> <branch>"
+    echo "This script takes two argument, namely the github repo you want to clone and the branch you want to checkout"
+    echo "Example: $0 git@github.com:bjollans/laralabs-automation.git main"
+}
+
+#If either the URL or the branch is empty, exit
+if [[ -z "$URL" ]]; then
+    help_and_exit
+fi
+
+if [[ -z "$BRANCH" ]]; then
+    help_and_exit
 fi
 
 if [[ ! -f .creds.json ]]; then
@@ -12,9 +31,16 @@ if [[ ! -f .creds.json ]]; then
     exit 1
 fi
 
+echo "Setting up dependencies"
+./install_basics.sh
+
 cat .creds.json | jq -r '.GithubSSH' > github_read_only_key
 chmod 400 github_read_only_key
 eval "$(ssh-agent -s)"
 ssh-add github_read_only_key
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone $URL
+cd $(echo $URL | sed 's|.*/||;s|.git||')
+git checkout $BRANCH
+cd -
+rm github_read_only_key
